4a5dca1b2bcba5aba893c09ae9d2c01d453e7a7f
BlingIceland <jon@dillon.is>
2026-04-10 11:09:17 +0000

    security: move Firebase config to env variables, add .env to gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYSAJp0r2UH63v7GHfDbJvavs6HaSeYIE",
-  authDomain: "vidreisn-gardabaer.firebaseapp.com",
-  projectId: "vidreisn-gardabaer",
-  storageBucket: "vidreisn-gardabaer.firebasestorage.app",
-  messagingSenderId: "793366722605",
-  appId: "1:793366722605:web:388126b14c6349ea6e60a4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
